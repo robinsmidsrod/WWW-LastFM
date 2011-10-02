ce6e534335fed3130bbec0ea394016b145da87f5
Robin Smidsrød <robin@smidsrod.no>
2011-10-02 16:02:37 +0200

    Added dependency on Config::Role and created some docs

--- lib/WWW/LastFM.pm
+++ lib/WWW/LastFM.pm
@@ -4,55 +4,15 @@ use namespace::autoclean;
 
 # ABSTRACT: Object-oriented interface to the Last.FM API version 2.0
 
-use File::HomeDir;
-use Path::Class::Dir;
-use Config::Any;
 use LWP::UserAgent;
 use WWW::LastFM::API::Geo;
 
 our $VERSION = "0.0.1";
 
-# Sometimes we like some extra debugging output
-has 'debug' => (
-    is      => 'ro',
-    isa     => 'Bool',
-    default => 0,
-);
-
-# Standard stuff to read our config file
-has 'config_file' => (
-    is         => 'ro',
-    isa        => 'Path::Class::File',
-    lazy_build => 1,
-);
-
-sub _build_config_file {
-    my ($self) = @_;
-    my $home = File::HomeDir->my_data;
-    my $conf_file = Path::Class::Dir->new($home)->file('.lastfm.ini');
-    return $conf_file;
-}
-
-# This is where our config file data ends up
-has 'config' => (
-    is         => 'ro',
-    isa        => 'HashRef',
-    lazy_build => 1,
-);
-
-sub _build_config {
-    my ($self) = @_;
-    my $cfg = Config::Any->load_files({
-        use_ext => 1,
-        files   => [ $self->config_file ],
-    });
-    foreach my $config_entry ( @{ $cfg } ) {
-        my ($filename, $config) = %{ $config_entry };
-        warn("Loaded config from file: $filename\n") if $self->debug;
-        return $config;
-    }
-    return {};
-}
+# Read configuration from ~/.lastfm.ini, available in $self->config
+has 'config_filename' => ( is => 'ro', isa => 'Str', lazy_build => 1 );
+sub _build_config_filename { '.lastfm.ini' }
+with 'Config::Role';
 
 # And here we have our api key and secret
 has 'api_key' => (
@@ -117,3 +77,32 @@ sub _build_geo {
 __PACKAGE__->meta->make_immutable();
 
 1;
+
+__END__
+
+=head1 SYNOPSIS
+
+    say join("\n",
+        map { $_->name_and_country }
+        WWW::LastFM->new->geo->get_metros()->metros->locations
+    );
+
+
+=head1 DESCRIPTION
+
+A partial client implementation of the L<Last.FM API|http://www.last.fm/api/>. Currently only the
+C<geo.getMetros> and C<geo.getEvents> are implemented.
+
+
+=head1 SEMANTIC VERSIONING
+
+This module uses semantic versioning concepts from L<http://semver.org/>.
+
+
+=head1 SEE ALSO
+
+=for :list
+* L<Moose>
+* L<XML::Rabbit>
+* L<Config::Role>
+* L<Last.FM API|http://www.last.fm/api/>
